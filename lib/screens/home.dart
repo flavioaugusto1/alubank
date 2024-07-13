99379ed991a1a3b03331ed9f082bcba62ac869f2
Flávio <f.augustosdn@gmail.com>
2024-07-12 21:55:00 -0300

    feat: criando tema e widget ColorDot

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:alubank/components/box_card.dart';
+import 'package:alubank/components/color_dot.dart';
 import 'package:alubank/components/sections/header.dart';
+import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -7,12 +9,12 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: <Widget>[
-          Header(),
+          const Header(),
           BoxCard(
-            boxContent: Text('Hello World'),
+            boxContent: ColorDot(color: ThemeColors.recentActivity['spent']),
           ),
         ],
       ),
